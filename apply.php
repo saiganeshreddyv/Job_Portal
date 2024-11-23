6e62938604ad43a5ef9b02d6459edc031b6a48fc
Vanipenta.Sai Ganesh Reddy <141120909+saiganeshreddyv@users.noreply.github.com>
2024-11-23 21:45:37 +0530

    apply.php

--- apply.php
+++ apply.php
@@ -1,8 +1,7 @@
 <?php
-// Assuming you have a variable $jobId that holds the job ID
-$jobId = 1;  // Replace with your actual method of obtaining the job ID
 
-// Redirect to jregister.php with job ID as a parameter
+$jobId = 1;  
+
 header("Location: jregister.php?jobId=$jobId");
 exit();
 ?>
