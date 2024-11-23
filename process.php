d10f98922a034cdc98e61fe13073b75418b895ed
Vanipenta.Sai Ganesh Reddy <141120909+saiganeshreddyv@users.noreply.github.com>
2024-11-23 21:53:14 +0530

    process.php

--- process.php
+++ process.php
@@ -1,18 +1,15 @@
 <?php
-// Establish a connection to the database
-$host = "localhost";  // e.g., "localhost"
+$host = "localhost"; 
 $username = "root";
 $password = "";
 $database = "reg";
 
 $conn = new mysqli($host, $username, $password, $database);
 
-// Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get values from the form
 $jobTitle = $_POST['jobTitle'];
 $companyName = $_POST['companyName'];
 $jobLocation = $_POST['jobLocation'];
@@ -20,9 +17,6 @@ $jobDescription = $_POST['jobDescription'];
 $skillsRequired = $_POST['skillsRequired'];
 $experienceRequired = $_POST['experienceRequired'];
 
-// Add more variables for additional attributes
-
-// Insert data into the database
 $sql = "INSERT INTO jobs (jobTitle, companyName, jobLocation, jobDescription, skillsRequired, experienceRequired) VALUES ('$jobTitle', '$companyName', '$jobLocation', '$jobDescription', '$skillsRequired', '$experienceRequired')";
 
 if ($conn->query($sql) === TRUE) {
@@ -31,7 +25,6 @@ if ($conn->query($sql) === TRUE) {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-// Close the database connection
 $conn->close();
 ?>
 
